521e9c6418b5705f540458a4dad67f62964165a6
Levi Porter <levi@levibrooke.com>
2018-09-22 17:02:56 -0700

    Add markup for index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import React from "react"
 
 import Layout from '../layouts';
+import levi from '../img/levi.jpg';
 
 export default class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <div>Hello world!</div>
+        <div className="wrapper">
+          <section className="intro">
+            <h1>Levi Porter</h1>
+            <h2>Fullstack developer based in Seattle.</h2>
+          </section>
+          <img id="home-img" src={levi} alt="Levi Porter on a mountain." />
+          <footer>
+            <a href="/about">About</a>
+            <a id="say-hello">Say Hello</a>
+            <a href="/work">Work</a>
+          </footer>
+        </div>
       </Layout>
     )
   }
