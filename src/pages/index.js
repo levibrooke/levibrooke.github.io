17cdf66cdd59f0c32889d996a992177f9c223304
Levi Porter <levi@levibrooke.com>
2018-09-24 14:00:20 -0700

    Revise img query, move inside containing section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,44 @@
-import React from "react"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 import Layout from '../layouts';
-import levi from '../img/levi.jpg';
 
-export default class Index extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <div className="wrapper">
-          <section className="intro">
-            <h1>Levi Porter</h1>
-            <h2>Fullstack developer based in Seattle.</h2>
-          </section>
-          <img id="home-img" src={levi} alt="Levi Porter on a mountain." />
-          <footer>
-            <a href="/about">About</a>
-            <a id="say-hello">Say Hello</a>
-            <a href="/work">Work</a>
-          </footer>
-        </div>
-      </Layout>
-    )
+const Index = (props) => (
+  <Layout>
+    <div className="wrapper">
+      <section className="intro">
+        <h1>Levi Porter</h1>
+        <h2>Fullstack developer based in Seattle.</h2>
+      </section>
+      <section className="home-img">
+        <Img 
+          alt="Levi Porter on a mountain."
+          fluid={props.data.imageOne.childImageSharp.fluid}
+        />
+      </section>
+      <footer>
+        <Link to="/about">About</Link>
+        <a id="say-hello">Say Hello</a>
+        <Link to="/work">Work</Link>
+      </footer>
+    </div>
+  </Layout>
+);
+
+export default Index;
+
+export const pageQuery = graphql`
+  query {
+    imageOne: file(relativePath: { eq: "levi.jpg" }) {
+      childImageSharp {
+        fluid(
+          maxWidth: 600
+        ) 
+        {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
   }
-} 
+`
