145faa269b041d933f9c318382021aaf411f8a25
Levi Porter <levi@levibrooke.com>
2018-09-24 18:27:38 -0700

    Implement color scheme switcher

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,34 +3,89 @@ import { Link, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
 import Layout from '../layouts';
+import levi from '../img/levi-grayscale.jpg';
 
-const Index = (props) => (
-  <Layout>
-    <div className="wrapper">
-      <section className="intro">
-        <h1>Levi Porter</h1>
-        <h2>Fullstack developer based in Seattle.</h2>
-      </section>
-      <section className="home-img">
-        <Img 
-          alt="Levi Porter on a mountain."
-          fluid={props.data.imageOne.childImageSharp.fluid}
-        />
-      </section>
-      <footer>
-        <Link to="/about">About</Link>
-        <a id="say-hello">Say Hello</a>
-        <Link to="/work">Work</Link>
-      </footer>
-    </div>
-  </Layout>
-);
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default Index;
+    this.state = {
+      theme: 'home',
+      wrapperColor: '#371722'
+    }
+  }
+
+  defaultHandler = () => {
+    this.setState({
+      theme: 'home',
+      wrapperColor: '#371722'
+    });
+  }
+
+  aboutHandler = () => {
+    this.setState({
+      theme: 'about',
+      wrapperColor: '#efe8d8'
+    })
+  }
+
+  workHandler = () => {
+    this.setState({
+      theme: 'work',
+      wrapperColor: '#97b8ae'
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const imgContainer = document.getElementById('home-img');
+    const wrapper = document.getElementById('wrapper');
+    if (this.state.theme !== prevState.theme) {
+      imgContainer.className = this.state.theme;
+      wrapper.style.background = this.state.wrapperColor;
+    }
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div id="wrapper">
+          <section className="intro">
+            <h1>Levi Porter</h1>
+            <h2>Fullstack developer based in Seattle.</h2>
+          </section>
+          <section id="home-img" className="home">
+            <img src={levi} />
+            {/* <Img 
+            alt="Levi Porter on a mountain."
+            fluid={props.data.imageOne.childImageSharp.fluid}
+          /> */}
+          </section>
+          <footer>
+            <Link
+              to="/about"
+              onMouseOver={this.aboutHandler}
+              onMouseLeave={this.defaultHandler}
+            >
+              About
+            </Link>
+            <a id="say-hello">Say Hello</a>
+            <Link 
+              to="/work"
+              onMouseOver={this.workHandler}
+              onMouseLeave={this.defaultHandler}
+            >
+              Work
+            </Link>
+          </footer>
+        </div>
+      </Layout>
+    )
+  }
+};
 
 export const pageQuery = graphql`
   query {
-    imageOne: file(relativePath: { eq: "levi.jpg" }) {
+    imageOne: file(relativePath: { eq: "levi-grayscale.jpg" }) {
       childImageSharp {
         fluid(
           maxWidth: 600
